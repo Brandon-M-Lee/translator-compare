fc264f10e39c0f58780d8c620610151bf5e5f6b6
Brandon-M-Lee <minjae.py@gmail.com>
2022-04-04 18:39:56 +0900

    번역 결과가 안나온다ㅠ

--- backend.py
+++ backend.py
@@ -7,5 +7,17 @@ except ImportError:
 import requests
 from urllib import parse
 
-# https://translate.google.co.kr/?hl=ko&sl=en&tl=ko&text=hello%20world&op=translate
-link_google_translator = 'https://translate.google.com/m?hl=en&sl=auto&tl=en&ie=UTF-8&prev=_m&q='
+sl = 'en'
+tl = 'ko'
+text = 'hello world'
+
+link_google_translator = f'https://translate.google.co.kr/?hl=ko&sl={sl}&tl={tl}&text={parse.quote(text)}&op=translate'
+link_papago = f'https://papago.naver.com/?sk={sl}&tk={tl}&hn=0&st={parse.quote(text)}'
+
+res_google = requests.get(link_google_translator)
+res_papago = requests.get(link_papago)
+
+soup_google = BeautifulSoup(res_google.text, 'html.parser')
+soup_papago = BeautifulSoup(res_papago.text, 'html.parser')
+
+print(soup_papago.prettify())
